160f6b184e81f6247d8eae0b7c2bec30b10a3083
Alexandre Arnt <falso@correio.com>
2013-06-05 23:14:19 -0300

    - Bugfix in refreshSystemTrayIcon code.

--- src/mainwindow_refresh.cpp
+++ src/mainwindow_refresh.cpp
@@ -57,9 +57,18 @@ void MainWindow::refreshAppIcon()
 void MainWindow::refreshSystemTrayIcon()
 {
   m_systemTrayIcon->setIcon(this->windowIcon());
+
   if(m_outdatedPackageList->count() == 0)
   {
     m_systemTrayIcon->setToolTip(StrConstants::getApplicationName());
+  }
+  else if (m_numberOfAvailablePackages == 1)
+  {
+    m_systemTrayIcon->setToolTip(StrConstants::getOneNewUpdate());
+  }
+  else if (m_numberOfOutdatedPackages > 1)
+  {
+    m_systemTrayIcon->setToolTip(StrConstants::getNewUpdates().arg(m_numberOfOutdatedPackages));
   }
 }
 
