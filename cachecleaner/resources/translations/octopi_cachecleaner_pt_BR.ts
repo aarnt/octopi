31363daccd13581768d9c88351e76bdae8a27299
Alexandre Arnt <falso@correio.com>
2015-02-16 16:25:10 -0200

    - Updated translations.

--- cachecleaner/resources/translations/octopi_cachecleaner_pt_BR.ts
+++ cachecleaner/resources/translations/octopi_cachecleaner_pt_BR.ts
@@ -4,7 +4,7 @@
     <message>
         <location filename="Projects/octopi/cachecleaner/ui/cachecleaner.ui" line="14"/>
         <source>Cache Cleaner - Octopi</source>
-        <translation>Limpeza de Cache - Octopi</translation>
+        <translation>Limpeza do Cache - Octopi</translation>
     </message>
     <message>
         <location filename="Projects/octopi/cachecleaner/ui/cachecleaner.ui" line="49"/>
