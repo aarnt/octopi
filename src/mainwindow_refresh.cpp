e7dddc4b65c6329b8d3049b3ff41b4875859fc5e
Alexandre Arnt <falso@correio.com>
2016-03-02 21:24:44 -0300

    - BugFix: test action before seting value.

--- src/mainwindow_refresh.cpp
+++ src/mainwindow_refresh.cpp
@@ -83,7 +83,7 @@ void MainWindow::refreshMenuTools()
     m_actionMirrorCheck->setVisible(true);
   }
   else
-    m_actionMirrorCheck->setVisible(false);
+    if (m_actionMirrorCheck) m_actionMirrorCheck->setVisible(false);
 
   if(UnixCommand::hasTheExecutable("plv"))
   {
