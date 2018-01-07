52e111f5ab53b5b60ac06ca1cd47c70e0aced435
Alexandre Arnt <falso@correio.com>
2018-01-06 23:15:25 -0200

    - BugFix: QProcess was not calling "pacman" command anymore (at least inside this service code). Had to add complete path to the command (a Qt 5.10 change).

--- notifier/pacmanhelper/pacmanhelper.cpp
+++ notifier/pacmanhelper/pacmanhelper.cpp
@@ -5,6 +5,8 @@
 #include <QProcess>
 #include <QDebug>
 
+#include <QFile>
+
 PacmanHelper::PacmanHelper(QObject *parent) :
   QObject(parent)
 {
@@ -28,8 +30,23 @@ void PacmanHelper::syncdb()
   env.insert("LANG", "us_EN");
   pacman.setProcessEnvironment(env);
 
-  QString command = "pacman -Syy";  //-Syy or -Sy - eternal doubt!
-  pacman.execute(command);
+  QStringList params;
+  params << "-Sy"; //-Syy or -Sy - eternal doubt!
+  QString command = "/usr/bin/pacman";
+  /*int code = */pacman.execute(command, params);
+
+  /*QString out;
+  out += "Return code of pacman command: " + QString::number(code) + "\n";
+  out += "Return of pacman command (stdout): " + pacman.readAllStandardOutput() + "\n";
+  out += "Return of pacman command (stderr): " + pacman.readAllStandardError() + "\n\n";
+
+  QString filename = QDir::homePath() + QDir::separator() + "output_pacmanhelper.txt";
+  QFile file( filename );
+  if ( file.open(QIODevice::ReadWrite) )
+  {
+    QTextStream stream( &file );
+    stream << out << endl;
+  }*/
 
   emit syncdbcompleted();
 }
