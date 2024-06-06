cb7b571dcc6354e7346ad182bf0da8a0eef68e2a
Alexandre Arnt <qtgzmanager@gmail.com>
2024-06-06 15:45:17 -0300

    - fix

--- src/pacmanexec.cpp
+++ src/pacmanexec.cpp
@@ -238,6 +238,11 @@ void PacmanExec::parsePacmanProcessOutput(const QString &output)
   msg.remove(QStringLiteral("F "));
   msg.remove(QRegularExpression(QStringLiteral("^\\($")));
 
+  msg.remove(QRegularExpression(QStringLiteral("Detected locale \"C\".+")));
+  msg.remove(QRegularExpression(QStringLiteral("Qt depends on a UTF-8 locale.+")));
+  msg.remove(QRegularExpression(QStringLiteral("libEGL warning.+")));
+  msg.remove(QRegularExpression(QStringLiteral("MESA: error:.*")));
+
   if (storeMsgCache) msgCache+=msg;
 
   if (msg.indexOf(QLatin1String(":: Synchronizing package databases...")) != -1)
@@ -455,7 +460,7 @@ void PacmanExec::parsePacmanProcessOutput(const QString &output)
     msg.remove(QRegularExpression(QStringLiteral("\\[K$")));
     msg.remove(QRegularExpression(QStringLiteral("Detected locale \"C\".+")));
     msg.remove(QRegularExpression(QStringLiteral("Qt depends on a UTF-8 locale.+")));
-    msg.remove(QRegularExpression(QStringLiteral("libEGL.*")));
+    msg.remove(QRegularExpression(QStringLiteral("libEGL warning.+")));
     msg.remove(QRegularExpression(QStringLiteral("MESA: error:.*")));
 
     msg = msg.trimmed();
