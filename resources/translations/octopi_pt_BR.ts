cdc2013cbb403ded93571d6569189428cabfb2c5
Alexandre Arnt <qtgzmanager@gmail.com>
2025-08-29 17:53:30 -0300

    - Updated tx

--- resources/translations/octopi_pt_BR.ts
+++ resources/translations/octopi_pt_BR.ts
@@ -282,7 +282,7 @@
     <message>
         <location filename="Projects/octopi/src/mainwindow_help.cpp" line="91"/>
         <source>An ignored package</source>
-        <translation>Pacote Ignorado</translation>
+        <translation>Pacote ignorado</translation>
     </message>
     <message>
         <location filename="Projects/octopi/src/mainwindow_help.cpp" line="93"/>
